3783cd207bef03e889757d57911fef89fee0b628
dahighsky <aakash.s@fyle.in>
2023-08-26 17:39:21 +0530

    Fiexes to font and Hero

--- Frontend/apna-aangan/src/components/FramedImage.jsx
+++ Frontend/apna-aangan/src/components/FramedImage.jsx
@@ -1,12 +1,13 @@
-const FramedImage = ({ src, alt, color, className }) => {
+const FramedImage = ({ src, alt, color, className, overlayB }) => {
   return (
     <div className={`framed-image ${className} relative`}>
+      <img src={src} alt={alt} className="h-full w-full object-cover p-1" />
+      {overlayB && <div className={`m-1 w-[99%] overlay absolute z-10 bottom-0 h-1/4 bg-gradient-to-t from-black/80 to-transparent`}></div>}
       <img
         src={`images/frame-${color}.svg`}
         alt="frame"
-        className="absolute max-w-full max-h-full min-w-full min-h-full z-10 top-0 left-0 "
+        className="absolute max-w-full max-h-full min-w-full min-h-full z-50 top-0 left-0"
       />
-      <img src={src} alt={alt} className="h-full w-full object-cover" />
     </div>
   );
 };
--- Frontend/apna-aangan/src/pages/home/Hero.jsx
+++ Frontend/apna-aangan/src/pages/home/Hero.jsx
@@ -5,13 +5,14 @@ const Hero = () => {
     <div className="hero flex flex-col justify-center items-center py-4">
       <div className="relative">
         <FramedImage
-          src="images/hero.jpg"
+          src="images/hero.jpeg"
           alt="hero"
           color="light"
-          className="h-[500px] w-auto"
+          className="h-[600px] w-auto"
+          overlayB
         />
       </div>
-      <div className="hero_content absolute z-50">
+      <div className="hero_content absolute z-50 bottom-[120px]">
         <h1 className="hero_title font-custom text-5xl text-theme-beige">
           WELCOME TO <br /> Apna Aangan
         </h1>
